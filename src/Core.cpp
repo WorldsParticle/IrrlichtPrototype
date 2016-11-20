5a860fd7ddbbfa3be45bf29e4a62131c6d44b2ba
thibaud-fdb <thibaud.fdb@gmail.com>
2016-11-20 17:15:10 +0100

    tweak again ..

--- src/Core.cpp
+++ src/Core.cpp
@@ -65,7 +65,7 @@ int Core::initModules()
 	device->setWindowCaption(L"WorldsParticle prototype");
 
 	_driver->setTextureCreationFlag(video::ETCF_ALWAYS_32_BIT, true);
-	_driver->setFog(video::SColor(0,125,125,125),EFT_FOG_LINEAR, WP_CAM_FARVALUE / 8, WP_CAM_FARVALUE, 0.5, true, true);
+	_driver->setFog(video::SColor(0,125,125,125),EFT_FOG_LINEAR, WP_CAM_FARVALUE / 8, WP_CAM_FARVALUE * 0.8, 0.5, true, true);
 
 	//
 	// CAMERA
